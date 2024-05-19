e690c6e6eb3b3a7f9eb06622c96b41683a802eca
Anubhab Bhattacharya <anubhabbhattacharya0710@gmail.com>
2024-05-19 22:16:43 +0530

    small fixes

--- effect.js
+++ effect.js
@@ -49,7 +49,7 @@ $('document').ready(function(){
 		setTimeout(function() {
 			setInterval(changeBackground, 3000);
 		}, 1000); // Adjust the delay as needed
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
+		$(this).fadeOut('slow').delay(3000).promise().done(function(){
 			$('#play').fadeIn('slow');
 		});
 	});
@@ -64,14 +64,14 @@ $('document').ready(function(){
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
 		$('body').css('backgroud-color','#FFF');
 		$('body').addClass('peach-after');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+		$(this).fadeOut('slow').delay(5000).promise().done(function(){
 			$('#bannar_coming').fadeIn('slow');
 		});
 	});
 
 	$('#bannar_coming').click(function(){
 		$('.bannar').addClass('bannar-come');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+		$(this).fadeOut('slow').delay(5000).promise().done(function(){
 			$('#balloons_flying').fadeIn('slow');
 		});
 	});
